76ac693cf5756660e9d0361ad2e397960eb14aba
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:04 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Spans.Readonly.ParameterInfos.Tests/ReadOnlySpanParameterInfosExtensionTests.cs
+++ test/Soenneker.Extensions.Spans.Readonly.ParameterInfos.Tests/ReadOnlySpanParameterInfosExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Spans.Readonly.ParameterInfos.Tests;
 
 public sealed class ReadOnlySpanParameterInfosExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
